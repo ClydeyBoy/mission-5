c457478e1207ef0c7967a4adb89f0fb1c8414848
Clyde D'Souza <clydedesouza89@gmail.com>
2023-01-10 23:43:34 +1300

    starting backend

--- backend/server.js
+++ backend/server.js
@@ -1,62 +1,29 @@
 const express = require('express');
-const server = express();
 const cors = require('cors');
-server.use(cors());
-server.use(express.json())
-const { connectToDb, getDb } = require('./db')
+const MongoClient = require('mongodb').MongoClient
 
-//db connection
-let db
+const app = express()
 
-connectToDb((error) => {
-    if (!error) {
-        server.listen(4000, () => {
-            console.log("listening to port 4000");
-        });
-        db = getDb()
-    }
+app.use(cors());
+app.use(express.json())
+let database
+
+app.listen(4000, () => {
+    MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true }, (error, result) => {
+        if (error) throw error
+        database = result.db('mission-5')
+        console.log('connection successful')
+    })
 })
-
-
-//routes
-server.get('/', (request, response) => {
-    let objects = []
-
-    db.collection('testing')
-        .find()
-        .forEach(object => { objects.push(object) })
-        .then(() => {
-            response.json(objects)
-        })
-})
-
-
-// const express = require('express');
-// const cors = require('cors');
-// const MongoClient = require('mongodb').MongoClient
-
-// const app = express()
-
-// app.use(cors());
-// app.use(express.json())
-// let database
-
-// app.listen(4000, () => {
-//     MongoClient.connect('mongodb://db/mission-5', { useNewUrlParser: true }, (error, result) => {
-//         if (error) throw error
-//         database = result.db('mission-5')
-//         console.log('connection successful')
-//     })
-// })
 
 // app.get('/', (request, response) => {
 //     response.send('Welcome to MongoDB API')
 // })
 
-// app.get('/api', (request, response) => {
-//     database.collection('testing').find({}).toArray((error, result) => {
-//         if (error) throw error
-//         response.send(result)
-//     })
-// })
+app.get('/propertypage', (request, response) => {
+    database.collection('Properties').find({}).toArray((error, result) => {
+        if (error) throw error
+        response.send(result)
+    })
+})
 
