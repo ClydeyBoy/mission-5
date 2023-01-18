93bb235961b6fb0ca4fa6c78a63a1b59e6ceb98b
Clyde D'Souza <clydedesouza89@gmail.com>
2023-01-18 16:54:07 +1300

    login and signup functionality

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+require('dotenv').config()
 const propertyRoutes = require('./routes/properties')
 const applicationRoutes = require('./routes/applications')
+const userRoutes = require('./routes/user')
 const mongoose = require('mongoose')
 const cors = require('cors')
 
@@ -19,6 +21,7 @@ app.use(express.json())
 //routes
 app.use('/propertypage', propertyRoutes)
 app.use('/propertypage', applicationRoutes)
+app.use('/user', userRoutes)
 
 //connect to db
 mongoose.connect('mongodb://localhost:27017/mission-5')
