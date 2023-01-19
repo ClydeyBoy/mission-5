28e2655b58d11f330307c10e515ee12fa5be4438
Clyde D'Souza <clydedesouza89@gmail.com>
2023-01-20 01:13:03 +1300

    adding property summary and chattels to backend

--- frontend/src/components/PropertyPageBody.js
+++ frontend/src/components/PropertyPageBody.js
@@ -38,35 +38,35 @@ function PropertyPageBody() {
             <CreateAccount trigger2={createAccount} setTrigger2={setCreateAccount}></CreateAccount>
             
             {/* importing the property photo from the useState hook */}
-            <img className='propertyPhoto' src={propertyData.property_image} alt=""></img>
+            <img className='propertyPhoto' src={propertyData.Property_image} alt=""></img>
 
             <div className='propertySummary'>
 
                 {/* importing the property address from the useState hook */}
-                <div className='propertyAddress'><b>{propertyData.address},</b> <br></br><b>{propertyData.suburb}</b></div>
+                <div className='propertyAddress'><b>{propertyData.Address},</b> <br></br><b>{propertyData.suburb}</b></div>
 
                 {/* importing the property room information from the useState hook */}
                 <div className='propertyRooms'>
-                    <div>{propertyData.bedrooms}<img className='bedroomIcon' src={bedroomIcon} alt=""></img></div>
-                    <div>{propertyData.bathrooms}<img className='bathroomIcon' src={bathroomIcon} alt=""></img></div>
-                    <div>{propertyData.carparks}<img className='parkingIcon' src={parkingIcon} alt=""></img></div>
+                    <div>{propertyData.Bedrooms}<img className='bedroomIcon' src={bedroomIcon} alt=""></img></div>
+                    <div>{propertyData.Bathrooms}<img className='bathroomIcon' src={bathroomIcon} alt=""></img></div>
+                    <div>{propertyData.Carparks}<img className='parkingIcon' src={parkingIcon} alt=""></img></div>
                 </div>
             </div>
 
            
             <div className='propertyDescription'>
                 
-                    <div>Presenting a one-bedroom apartment that stands out from the rest. Walk in and the first thing you'll notice is the EPIC high ceiling stud. It creates a sense of spaciousness and style that you just don't find in most apartments.</div>
+                    <div>{propertyData.Property_summary_para1}</div>
                     <br></br>
-                    <div>The tidy kitchen and bathroom are in good condition and the bedroom is a generous size. The north facing balcony gives you plenty of sun and a spot for the summer BBQ, and ensures the apartment is bathed in light.</div>
+                    <div>{propertyData.Property_summary_para2}</div>
                     <br></br>
-                    <div>There's easy value to add if you want to polish it further and make something truly special. Or if you are a first home buyer looking for a city fringe pad, then this one is quite simply a cut above the rest.</div>
+                    <div>{propertyData.Property_summary_para3}</div>
                     <br></br>
-                    <div>The apartment comes with not 1, not 2, but 3 carparks, as well as being adjacent to the train line and city bus routes.</div>
+                    <div>{propertyData.Property_summary_para4}</div>
                     <br></br>
-                    <div>All this and located in what is unquestionably Auckland's hippest little suburb, Morningside. Take your pick from the cafes, craft beer spots, event venues and eateries all on your doorstep. The gym is just next door and it's a quick few minutes down the road to Westfield St Luke's for a spot of shopping.</div>
+                    <div>{propertyData.Property_summary_para5}</div>
                     <br></br>
-                    <div>This apartment is a cracker and worth putting at the top of your Xmas list.</div>
+                    <div>{propertyData.Property_summary_para6}</div>
             </div>
 
             {/* creating buttons to display  the signup and tenancy application popup forms when clicked */}
@@ -82,27 +82,28 @@ function PropertyPageBody() {
 
             {/* importing the property data from the useState hook */}
             <div className='propertyInfoColumn1'>
-                <div>Property type: {propertyData.property_type}</div>
-                <div>Rent: {propertyData.rent} per week</div>
-                <div>Available: {propertyData.available}</div>
-                <div>Amenities: {propertyData.amenities}</div>
-                <div>Bedrooms: {propertyData.bedrooms}</div>
-                <div>Bathrooms: {propertyData.bathrooms}</div>
-                <div>Parking: {propertyData.carparks}</div>
-                <div>Pets Allowed: {propertyData.pets_allowed}</div>
-                <div>Property ID: {propertyData.property_id}</div>
+                <div>Property type: {propertyData.Property_type}</div>
+                <div>Rent: {propertyData.Rent} per week</div>
+                <div>Available: {propertyData.Available}</div>
+                <div>Amenities: {propertyData.Amenities}</div>
+                <div>Bedrooms: {propertyData.Bedrooms}</div>
+                <div>Bathrooms: {propertyData.Bathrooms}</div>
+                <div>Parking: {propertyData.Carparks}</div>
+                <div>Pets Allowed: {propertyData.Pets_allowed}</div>
+                <div>Property ID: {propertyData.Property_id}</div>
             </div>
 
            
             <div className='propertyInfoColumn2'>
                 
-                    <div>Washing machine</div>
-                    <div>Dryer</div>
-                    <div>Extractor Fan</div>
-                    <div>Shelving in Lounge</div>
-                    <div>Chairs and table on balcony</div>
-                    <div>Wicker coffee table in lounge</div>
-                    <div>Sofa</div>
+                    <div>{propertyData.Chattels1}</div>
+                    <div>{propertyData.Chattels2}</div>
+                    <div>{propertyData.Chattels3}</div>
+                    <div>{propertyData.Chattels4}</div>
+                    <div>{propertyData.Chattels5}</div>
+                    <div>{propertyData.Chattels6}</div>
+                    <div>{propertyData.Chattels7}</div>
+                    <div>{propertyData.Chattels8}</div>
             </div>
 
             <div className='carouselHeader'>
@@ -111,10 +112,10 @@ function PropertyPageBody() {
 
             {/* importing the property manager info from the useState hook */}
             <div className='propertyManagerCard'>
-                <img className='propertyManagerImg' src={propertyData.property_manager_image} alt=''></img>
+                <img className='propertyManagerImg' src={propertyData.Property_manager_photo} alt=''></img>
                 <div className='propertyManagerText'>
-                    <div className='propertyManagerName'>{propertyData.property_manager_name}<br></br>Property Manager</div>
-                    <div className='propertyManagerNumber'>{propertyData.property_manager_number}</div>
+                    <div className='propertyManagerName'>{propertyData.Property_manager_name}<br></br>Property Manager</div>
+                    <div className='propertyManagerNumber'>{propertyData.Property_manager_number}</div>
                     <div className='emailBtn'>Email Agent</div>
                 </div>
             </div>
